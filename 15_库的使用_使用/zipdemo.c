1542896910779e78f279db93bd9ff8a7045f829d
Elephant Liu <lexdene@sohu.com>
2014-08-19 21:30:11 +0800

    mv 15

--- 15_库的使用_使用/zipdemo.c
+++ 15_库的使用_使用/zipdemo.c
@@ -9,26 +9,25 @@ int main(int argc, char *argv[]){
 	struct zip* zipObj = NULL;
 	int num_entries = 0;
 	int i;
-	
+
 	if( argc < 2){
 		usage();
 		return 1;
 	}
-	
-	
+
 	zipObj = zip_open(argv[1],0,&errorp);
 	if( NULL == zipObj ){
 		printf("open zip file failed , error code : %d\n",errorp);
 		return 2;
 	}
-	
+
 	num_entries = zip_get_num_files(zipObj) ;
 	for( i = 0 ; i < num_entries ; ++i ){
 		printf("%s\n",zip_get_name(zipObj,i,0) );
 	}
-	
+
 	zip_close(zipObj);
 	zipObj = NULL ;
-	
+
 	return 0;
 };
